b3701c58ad9c1e0b0abdabae4a1dec131a9ed530
GustavoMacrini <gustavo.godencio@hotmail.com>
2022-09-21 19:50:17 -0300

    Drawer básico

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:lol_matching/model/tela_modelo.dart';
 
-void main(){
+void main() {
   runApp(
     MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'LoL Matching',
-      initialRoute: 't1',
-      routes: {
-        't1' :(context) => TelaModelo(),
-      },
-    )
-  );
+    debugShowCheckedModeBanner: false,
+    title: 'LoL Matching',
+    initialRoute: 't1',
+    routes: {
+      't1': (context) => TelaModelo(),
+    },
+  ));
 }
--- lib/model/tela_modelo.dart
+++ lib/model/tela_modelo.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -11,34 +9,42 @@ class TelaModelo extends StatefulWidget {
 }
 
 class _TelaModeloState extends State<TelaModelo> {
-
-@override
-void initState(){
-  super.initState();
-  SystemChrome.setPreferredOrientations([
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
       DeviceOrientation.landscapeRight,
       DeviceOrientation.landscapeLeft,
-  ]);
-}
+    ]);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(248,245,240,1),
-      body: Container(
-        width: MediaQuery.of(context).size.width*0.15,
-        child: Drawer(
-          backgroundColor: Color.fromRGBO(249, 241, 227, 1),
-          child: Column(
-            children: [
-            Text('Item 1'),
-            Text('Item 2'),
-            Text('Item 3'),
-            Text('Item 4'),
-          ],),
-
-        ),
+      backgroundColor: Color.fromRGBO(248, 245, 240, 1),
+      appBar: AppBar(
+        title: Text('Teste'),
       ),
+      body: const Center(
+        child: Text('My Page!'),
+      ),
+      drawer: Drawer(
+          backgroundColor: Color.fromRGBO(249, 241, 227, 1),
+          width: MediaQuery.of(context).size.width * 0.3,
+          child: Container(
+            alignment: Alignment.center,
+            child: ListView(padding: EdgeInsets.all(24), children: [
+              ListTile(
+                  leading: Icon(
+                    Icons.person_add,
+                    color: Colors.blue,
+                  ),
+                  title: Text('Amigos'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  })
+            ]),
+          )),
     );
   }
 }
