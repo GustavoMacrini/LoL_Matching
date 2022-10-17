833b8ce09b958e0070e6b65a3aa5adc88582648d
GustavoMacrini <gustavo.godencio@hotmail.com>
2022-10-17 13:23:43 -0300

    Criação de widgets para filtros

--- lib/model/tela_pesquisa.dart
+++ lib/model/tela_pesquisa.dart
@@ -1,57 +1,72 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:lol_matching/model/widgets/filtro_widget.dart';
 import 'package:lol_matching/model/variaveis.dart';
-
-const List<String> list = <String>[ 'One', 'Two', 'Three', 'Four'];
+import 'package:lol_matching/model/widgets/filtro_widget_champ.dart';
+import 'package:lol_matching/model/widgets/filtro_widget_tipo.dart';
 
 class TelaPesquisa extends StatefulWidget {
-  const TelaPesquisa({Key? key}) : super(key: key);
+   
+      
+  TelaPesquisa({Key? key}) : super(key: key);
 
   @override
   State<TelaPesquisa> createState() => _TelaPesquisaState();
 }
 
 class _TelaPesquisaState extends State<TelaPesquisa> {
+
   @override
   Widget build(BuildContext context) {
-    String dropdownValue = list.first;
     return Scaffold(
-      
+     
       backgroundColor: secondaryColor,
       body:
       
       Container(
-        child: Column(
-          
+        margin: EdgeInsets.fromLTRB(30, 30, 30, 0),
+        child: Column(          
           children: [
-            DropdownButton<String>(
-      value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? value) {
-        // This is called when the user selects an item.
-        setState(() {
-          dropdownValue = value!;
-        });
-      },
-      items: list.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    )
-        ]),)
 
-    );
+            //FILTROS
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    DropPageChamp(),
+                    DropPageFiltro(),
+                  ],
+                ),
+              ],
+            ),
+
+            //BOTAO
+            botao("PESQUISAR"),
+
+
+        ]),
+      ),
+    );    
+  }
+
+  botao(titulo){
+    return Container(
+      width: 120,
+      height: 38,
+      child: ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: primaryColor,        
+        ),
+      onPressed: (){               
+      }, 
+      child: 
+      Text(
+        titulo, 
+        style: TextStyle(
+          color: secondaryColor, 
+          fontSize: 16
+          ),
+      ),
+      ),
+      );
   }
 }
---
+++ lib/model/widgets/filtro_widget_champ.dart
@@ -0,0 +1,57 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class DropPageChamp extends StatelessWidget {
+  final dropValue = ValueNotifier('');
+  final dropOpcoes = [
+    'Aatrox',
+    'Syndra',
+    'Khazix',
+    'Ziggs',
+    'Gragas',
+    ];
+  
+    DropPageChamp(
+      {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {     
+    return Container(
+      child: ValueListenableBuilder(
+        valueListenable: dropValue, builder: ((context, String value, _) {
+          return SizedBox(
+            width: 230,
+            child: DropdownButtonFormField<String>(
+              isExpanded: true,
+              hint: const Text('Selecione a opção'),
+              decoration:               
+              InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6),
+                ),
+                fillColor: Colors.white,
+                filled: true
+              ),
+              
+              value: (value.isEmpty) ? null : value,
+              onChanged: (escolha) => dropValue.value = escolha.toString(),
+              items: dropOpcoes.map((op) => DropdownMenuItem(
+                value: op,
+                child: 
+                Row(children: [
+                  Image(image: NetworkImage("http://ddragon.leagueoflegends.com/cdn/12.19.1/img/champion/$op.png"),
+                  width: 40,
+                  height: 40,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 30),
+                    child: Text(op)),
+                ],)
+              ),).toList(),
+            ),
+          );
+        } ),)
+    );
+  }
+}
